a6442a7d92a7f258a73cf0c9b8271b352a7aa703
hot7585325 <hot7585325@gmail.com>
2025-09-11 14:21:13 +0800

    更改介面

--- src/js/PlaneTrackindMode.js
+++ src/js/PlaneTrackindMode.js
@@ -1,10 +1,15 @@
-  // 取得 <a-scene> 元素
-    const sceneEl = document.querySelector('a-scene');
 
-    // 當 A-Frame 場景載入完成後
-document.getElementById("Name_button").addEventListener('click', () => {
-      // 自動進入 VR 模式，這會觸發 AR 模式
-      // 瀏覽器會自動彈出要求相機權限的提示
-      console.log("RUP4BJ4")
-      sceneEl.enterVR();
+document.getElementById('start-ar').addEventListener('click', async () => {
+  if (navigator.xr) {
+    const session = await navigator.xr.requestSession('immersive-ar', {
+      requiredFeatures: ['hit-test']
     });
+
+    renderer.xr.setSession(session);
+  } else {
+    alert('你的裝置不支援 WebXR AR 模式');
+  }
+});
+
+
+  document.getElementById("Home_button").addEventListener("click", function () {window.location.href='index.html' })
